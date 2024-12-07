b6b759d6594d8dfa422c86c5d3fd960fb0cd2fcd
June <sanjunah16@gmail.com>
2024-12-07 17:12:06 +0900

    .

--- app.py
+++ app.py
@@ -4,12 +4,12 @@ import intro
 import complete
 
 # Streamlit 페이지 구성
-st.set_page_config(page_title="ReadingAssistantExperiment", page_icon="🌟")
+st.set_page_config(page_title="SJLNP2024", page_icon="🎊")
 
 # 페이지 전환
 if "page" not in st.session_state:
     st.session_state["page"] = "intro"
-    
+
 if st.session_state["page"] == "intro":
     intro.intro_page()
 # elif st.session_state["page"] == "experiment":
