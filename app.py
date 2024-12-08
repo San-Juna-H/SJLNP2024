bf1a6753913f25a351ffb0fdacf27d4c1340d905
June <sanjunah16@gmail.com>
2024-12-08 16:19:24 +0900

    .

--- app.py
+++ app.py
@@ -9,10 +9,3 @@ st.set_page_config(page_title="SJLNP2024", page_icon="🎊")
 # 페이지 전환
 if "page" not in st.session_state:
     st.session_state["page"] = "intro"
-
-if st.session_state["page"] == "intro":
-    intro.intro_page()
-# elif st.session_state["page"] == "experiment":
-#     question.question_page()
-elif st.session_state["page"] == "completion":
-    complete.completion_page()
